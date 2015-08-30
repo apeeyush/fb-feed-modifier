3ce58ee8e6e4020fdcd596ff337342a7497691d9
apeeyush <peeyushagarwal1994@gmail.com>
2015-08-30 14:17:09 +0530

    Create separate div for word and meaning

--- kill.js
+++ kill.js
@@ -6,33 +6,34 @@ function getRandomInt(min, max) {
 
 function blockAndDisplay() {
     var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]');
-    var message = $('#distracted');
-
+    var word_div = $('#word');
+    var meaning_div = $('#meaning');
     if (feed.length == 0) {
-        message.remove();
-    } else if (message.length == 0) {
+        word_div.remove();
+        meaning_div.remove();
+    }else if (word_div.length == 0) {
         word_dict = getRandomWord();
         word = $('<h1>')
-            .attr('id', 'distracted')
+            .attr('id', 'word')
             .text(word_dict['word'])
             .css('font-size', '30px')
             .css('font-family', "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif")
             .css('position', 'relative')
-            .css('top', '75px');
+            .css('top', '95px');
         meaning = $('<h3>')
-            .attr('id', 'distracted')
+            .attr('id', 'meaning')
             .text(word_dict['meaning'])
             .css('font-size', '20px')
             .css('font-family', "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif")
             .css('position', 'relative')
-            .css('top', '75px');
+            .css('top', '105px');
         $('[data-location=maincolumn]').append(word);
         $('[data-location=maincolumn]').append(meaning);
-     }
-
+    }
     feed.children().remove();
     $('.ticker_stream').remove();
     $('.ego_column').remove();
+    // $('#pagelet_composer').remove();
     $('#pagelet_games_rhc').remove();
     $('#pagelet_trending_tags_and_topics').remove();
     $('#pagelet_canvas_nav_content').remove();
